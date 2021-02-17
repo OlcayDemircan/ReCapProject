d5dec7d52c5698bcc1ad744a45952050dfe7bcfb
Olcay DEMIRCAN <olcaydemircann@gmail.com>
2021-02-17 16:02:53 +0100

    EntityManagerBase Fault

--- Business/Concrete/BrandManager.cs
+++ Business/Concrete/BrandManager.cs
@@ -12,48 +12,48 @@ using System.Text;
 
 namespace Business.Concrete
 {
-    public class BrandManager : IBrandService
+    public class BrandManager : EntityManagerBase<Brand,ReCapDatabaseContext>
     {
-        IBrandDal _iBrandDal;
+        //IBrandDal _iBrandDal;
 
-        public BrandManager(IBrandDal iBrandDal)
-        {
-            _iBrandDal = iBrandDal;
-        }
+        //public BrandManager(IBrandDal iBrandDal)
+        //{
+        //    _iBrandDal = iBrandDal;
+        //}
 
-        public IResult Add(Brand entity)
-        {
-            _iBrandDal.Add(entity);
-            return new SuccessResult(Messages.Added);
-        }
+        //public IResult Add(Brand entity)
+        //{
+        //    _iBrandDal.Add(entity);
+        //    return new SuccessResult(Messages.Added);
+        //}
 
-        public IResult Delete(Brand entity)
-        {
-            _iBrandDal.Delete(entity);
-            return new SuccessResult(Messages.Deleted);
-        }
+        //public IResult Delete(Brand entity)
+        //{
+        //    _iBrandDal.Delete(entity);
+        //    return new SuccessResult(Messages.Deleted);
+        //}
 
-        public IDataResult<Brand> Get(Expression<Func<Brand, bool>> filter)
-        {
-            using (ReCapDatabaseContext context = new ReCapDatabaseContext())
-            {
-                return new SuccessDataResult<Brand>(context.Set<Brand>().SingleOrDefault(filter));
-            }
-        }
+        //public IDataResult<Brand> Get(Expression<Func<Brand, bool>> filter)
+        //{
+        //    using (ReCapDatabaseContext context = new ReCapDatabaseContext())
+        //    {
+        //        return new SuccessDataResult<Brand>(context.Set<Brand>().SingleOrDefault(filter));
+        //    }
+        //}
 
-        public IDataResult<List<Brand>> GetAll(Expression<Func<Brand, bool>> filter = null)
-        {
-            if (DateTime.Now.Hour == 22)
-            {
-                return new ErrorDataResult<List<Brand>>(Messages.MaintenanceTime);
-            }
-            return new SuccessDataResult<List<Brand>>(_iBrandDal.GetAll(), Messages.Listed);
-        }
+        //public IDataResult<List<Brand>> GetAll(Expression<Func<Brand, bool>> filter = null)
+        //{
+        //    if (DateTime.Now.Hour == 22)
+        //    {
+        //        return new ErrorDataResult<List<Brand>>(Messages.MaintenanceTime);
+        //    }
+        //    return new SuccessDataResult<List<Brand>>(_iBrandDal.GetAll(), Messages.Listed);
+        //}
 
-        public IResult Update(Brand entity)
-        {
-            _iBrandDal.Update(entity);
-            return new SuccessResult(Messages.Updated);
-        }
+        //public IResult Update(Brand entity)
+        //{
+        //    _iBrandDal.Update(entity);
+        //    return new SuccessResult(Messages.Updated);
+        //}
     }
 }
